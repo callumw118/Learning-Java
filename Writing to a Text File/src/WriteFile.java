8da965057652a9fe89ce2e918cc8f0f04a8a34ec
Callum Wolfe <59138312+callumw118@users.noreply.github.com>
2020-02-13 21:47:34 +0000

    Writing to a Text File

---
+++ Writing to a Text File/src/WriteFile.java
@@ -0,0 +1,11 @@
+import java.io.FileWriter; //FileWriter class allows you to specify overwriting everything or starting at the end
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class WriteFile {
+
+    public static void main(String[] args) {
+
+    }
+
+}
